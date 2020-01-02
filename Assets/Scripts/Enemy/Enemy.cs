4bd7851181b7a15a6a69303b50375c35619c6821
AlecFaye <kelvintran.lim@outlook.com>
2020-01-02 02:16:35 -0500

    Cleaned up enemy code using inheritance and skeleton setup

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -10,10 +10,65 @@ public abstract class Enemy : MonoBehaviour
 
     [SerializeField] protected Transform pointA, pointB;
 
-    public virtual void Movement()
-    {
+    protected Vector3 _currentTarget;
+    protected Animator _animator;
+    protected bool _facingRight = true;
 
+    private void Start()
+    {
+        Init();
     }
 
-    public abstract void Update();
+    public virtual void Init()
+    {
+        _currentTarget = pointB.position;
+        _animator = GetComponentInChildren<Animator>();
+    }
+
+    public virtual void Update()
+    {
+        if (_animator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+            return;
+
+        CheckDirection();
+
+        Movement();
+    }
+
+    public virtual void CheckDirection()
+    {
+        if (_currentTarget == pointA.position && _facingRight)
+        {
+            Flip();
+            _facingRight = false;
+        }
+        else if (_currentTarget == pointB.position && !_facingRight)
+        {
+            Flip();
+            _facingRight = true;
+        }
+    }
+
+    public void Flip()
+    {
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    public virtual void Movement()
+    {
+        if (transform.position == pointA.position)
+        {
+            _currentTarget = pointB.position;
+            _animator.SetTrigger("Idle");
+        }
+        else if (transform.position == pointB.position)
+        {
+            _currentTarget = pointA.position;
+            _animator.SetTrigger("Idle");
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, _currentTarget, speed * Time.deltaTime);
+    }
 }
--- Assets/Scripts/Enemy/MossGiant.cs
+++ Assets/Scripts/Enemy/MossGiant.cs
@@ -4,60 +4,9 @@ using UnityEngine;
 
 public class MossGiant : Enemy
 {
-    private Vector3 _currentTarget;
-    private Animator _animator;
-    private bool _facingRight = true;
-
-    private void Start()
+    // Used for initialization
+    public override void Init()
     {
-        _animator = GetComponentInChildren<Animator>();
-        _currentTarget = pointB.position;
-    }
-
-    public override void Update()
-    {
-        if (_animator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
-            return;
-
-        CheckDirection();
-
-        Movement();
-    }
-
-    private void CheckDirection()
-    {
-        if (_currentTarget == pointA.position && _facingRight)
-        {
-            Flip();
-            _facingRight = false;
-        }
-        else if (_currentTarget == pointB.position && !_facingRight)
-        {
-            Flip();
-            _facingRight = true;
-        }
-    }
-
-    private void Movement()
-    {
-        if (transform.position == pointA.position)
-        {
-            _currentTarget = pointB.position;
-            _animator.SetTrigger("Idle");
-        }
-        else if (transform.position == pointB.position)
-        {
-            _currentTarget = pointA.position;
-            _animator.SetTrigger("Idle");
-        }
-
-        transform.position = Vector3.MoveTowards(transform.position, _currentTarget, speed * Time.deltaTime);
-    }
-
-    private void Flip()
-    {
-        Vector3 theScale = transform.localScale;
-        theScale.x *= -1;
-        transform.localScale = theScale;
+        base.Init();
     }
 }
